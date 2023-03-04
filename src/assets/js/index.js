6cf88e480da5d65c95ce568dcf0eaac5d829c799
Txrigxn <96521135+Txrigxn@users.noreply.github.com>
2023-03-04 15:25:45 +0100

    feat: nav bar and theme mod add

---
+++ src/assets/js/index.js
@@ -0,0 +1,46 @@
+// Toogle Menu
+
+const toggleMenu = document.getElementById('toggle-menu');
+console.log(toggleMenu);
+const toggleMenuIcon = toggleMenu.querySelector('img');
+const menuElem = document.getElementById('menu');
+
+toggleMenu.addEventListener('click', ()=> {
+    menuElem.classList.toggle('translate-y-[-200%]');
+    changeMenuIcon();
+});
+
+// Close Menu When we are clicking on Menu List
+
+const navLinks = document.querySelectorAll('.nav-link');
+for(const navlink of navLinks) {
+    navlink.addEventListener('click', ()=>{
+        menuElem.classList.add('translate-y-[-200%]');
+        changeMenuIcon();
+    });
+}
+
+function changeMenuIcon() {
+    const isContainTranslate = menuElem.classList.contains('translate-y-[-200%]')
+    if(!isContainTranslate) {
+        toggleMenuIcon.src = './assets/images/icon-menu-close.svg'
+    }else{
+        toggleMenuIcon.src = './assets/images/icon-menu.svg'
+    }
+}
+
+// Toggle Dark Mode
+
+const html = document.querySelector('html');
+const toggleTheme = document.getElementById('toggle-theme');
+const toggleThemeIcon = toggleTheme.querySelector('img');
+toggleThemeIcon.addEventListener('click', ()=>{
+    html.classList.toggle('dark');
+    const isDark = html.classList.contains('dark');
+    if(!isDark){
+        toggleThemeIcon.src = './assets/images/icon-dark-mode.svg'
+    }else{
+        toggleThemeIcon.src = './assets/images/icon-light-mode.svg'
+    }
+})
+
